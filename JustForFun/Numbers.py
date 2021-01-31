b9806666993d457b2daf62accafe2ad86914cd1b
Ryan Cox <G00398251@gmit.ie>
2021-01-31 21:01:09 +0000

    Practice code

---
+++ JustForFun/Numbers.py
@@ -0,0 +1,8 @@
+# Reading in a number as a string, converting it to a int. 
+number = int(input("Enter a number?"))
+
+# Adding a number to that int and creating a int as a variable "new number"
+newnumber = (number + 154)
+
+# Formating to place the int into a sentence for print
+print("Let add 154 to {} and make that equal {}" .format (number,newnumber))
